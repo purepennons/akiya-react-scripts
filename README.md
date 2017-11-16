e5982b2ec293b6c1cc4c4abe09fc746625edd9b3
Chiahao Lin <y80811@gmail.com>
2017-11-16 18:49:16 +0800

    feat: support react component building environment by `akiya-reac t-component-scripts` flag

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ My react development environment.
 Integrate with [create-react-app](https://github.com/facebookincubator/create-react-app) and [akiya-react-scripts](https://github.com/purepennons/akiya-react-scripts).
 
 ## Usage
+
+### Preparement
 ```bash
 $ npm install -g create-react-app
+```
+### React App
+```bash
 $ create-react-app <APP_NAME> --scripts-version akiya-react-scripts
+```
+
+### React Component
+```bash
+$ create-react-app <APP_NAME> --scripts-version akiya-react-component-scripts
 ```
--- packages/akiya-react-component-scripts/template/styleguide.config.js
+++ packages/akiya-react-component-scripts/template/styleguide.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
   // require: [path.join(__dirname, 'src/index.css')],
-  components: 'src/**/[A-Z]*.js',
+  components: 'src/react-component-node-modules/**/[A-Z]*.js',
   webpackConfig(env) {
     if (env === 'production')
-      return require('./node_modules/akiya-react-scripts/config/webpack.config.prod.js');
+      return require('./node_modules/akiya-react-component-scripts/config/webpack.config.prod.js');
     else
-      return require('./node_modules/akiya-react-scripts/config/webpack.config.dev.js');
+      return require('./node_modules/akiya-react-component-scripts/config/webpack.config.dev.js');
   },
 };
